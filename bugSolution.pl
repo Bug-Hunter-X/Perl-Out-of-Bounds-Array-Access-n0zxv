82ea7e3b8c04308aeeefd358d0197f44cd575474
BugHunter-X <askzenai@gmail.com>
2024-12-19 19:12:05 +0100

    Add bugSolution.pl file

---
+++ bugSolution.pl
@@ -0,0 +1,1 @@
+my @array = (1, 2, 3);my $size = @array; # Get the array sizeif ($size > 0) {  my $index = 2; #Check index is within bounds.    if ($index >= 0 && $index < $size){my $element = $array[$index]; # Accessing a valid elementprint "Element at index $index: $element\n"} else {print "Index out of bounds\n"} } else {print "Array is empty\n"} 
